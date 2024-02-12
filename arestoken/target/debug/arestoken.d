b381eb7e3558806fb21366e820d9b5a4c663b519
WaqasAyubShah <smwaqas.ayubshah@gmail.com>
2024-02-12 12:45:22 -0700

    ARES token without lottery & fix the smart contract

---
+++ arestoken/target/debug/arestoken.d
@@ -0,0 +1,1 @@
+F:\Clients\ work\adeel\ bhai\ARES\ Token\arestoken\target\debug\arestoken.exe: F:\Clients\ work\adeel\ bhai\ARES\ Token\arestoken\src\main.rs
---
+++ arestoken/target/debug/deps/arestoken.d
@@ -0,0 +1,5 @@
+F:\Clients work\adeel bhai\ARES Token\arestoken\target\debug\deps\arestoken.exe: src\main.rs
+
+F:\Clients work\adeel bhai\ARES Token\arestoken\target\debug\deps\arestoken.d: src\main.rs
+
+src\main.rs:
